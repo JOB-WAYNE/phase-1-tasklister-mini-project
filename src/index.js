05fead5b52d662e1aa48372ad82cfd541ab0a445
jobwayne <otienojobwayneotieno@gmail.com>
2024-10-06 22:40:53 +0300

    task-lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+document.addEventListener("DOMContentLoaded",()=>
+{ let form = document.querySelector("#create-task-form");
+    form.addEventListener("submit",(e)=>
+    { e.preventDefault();
+        createTodo(e.target["mew-task-description"].value);
+        form.reset();
+    });
 });
+ 
+function createTodo(todo)
+{
+    let li =
+    document.createElement("li");
+    let btn =
+    document.createElement("button");
+    btn.addEventListener("click", handleDelete);
+    btn.textContent = "Delete";
+    li.textContent=todo;
+    li.appendChild(btn);
+    document.querySelector("#tasks").appendChild(li);
+}
+function handleDelete(e){
+    e.target.parentNode.removed();
+}
